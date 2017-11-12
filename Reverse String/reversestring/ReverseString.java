df21dfa480de7776ecac7d171591cb1fb8d863fc
matmaras <matimaras@gmail.com>
2017-11-12 21:55:42 +0100

    Reverse String 2

--- Reverse String/reversestring/ReverseString.java
+++ Reverse String/reversestring/ReverseString.java
@@ -1,14 +1,53 @@
 package reversestring;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
 public class ReverseString {
 
 	public static void main (String[] args) {
 		
-		System.out.println(reverseString1("a."));
+		System.out.println(reverseString5("hello"));
 	}
 	
     public static String reverseString(String s) {
     	return new StringBuilder(s).reverse().toString();
+    }
+    
+    public static String reverseString5(String s) {
+        char[] word = s.toCharArray();
+        
+        List<Character> list = new LinkedList<>();
+
+        for(char ch : word) {
+        	list.add(ch);
+        }
+        
+        ListIterator<Character> liEnd = list.listIterator(list.size());
+        ListIterator<Character> liBegin = list.listIterator();
+        
+        while(liEnd.hasPrevious() && liBegin.hasNext() && liEnd.previous() != liBegin.next()) {
+        	  Character tmp = liEnd.previous();
+        	  liEnd.set(liBegin.next());
+        	  liBegin.set(tmp);
+        	}
+        
+        return list.toString();
+    }
+    
+    public static String reverseString3(String s) {
+        char[] word = s.toCharArray();
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            word[i] = (char) (word[i] ^ word[j]);
+            word[j] = (char) (word[i] ^ word[j]);
+            word[i] = (char) (word[i] ^ word[j]);
+            i++;
+            j--;
+        }
+        return new String(word);
     }
     
     public static String reverseString1(String s) {
